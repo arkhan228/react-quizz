8b584d05125b2c9f91cf17b2e3a3b67368a9a3dd
Abdul Rhman <ark573235@gmail.com>
2024-03-14 16:24:55 +0530

    minor error in progress bar fixed

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,12 +1,15 @@
 import { useQuiz } from '../contexts/QuizContext';
 
 export default function Progress() {
-  const { curQuest, numQuestions, curPoints, numPoints, answer } = useQuiz();
+  const { curQuest, numQuestions, curPoints, numPoints, answers } = useQuiz();
   console.log(numQuestions);
 
   return (
     <header className='progress'>
-      <progress value={curQuest + Number(answer !== null)} max={numQuestions} />
+      <progress
+        value={curQuest + Number(answers[curQuest] !== null)}
+        max={numQuestions}
+      />
       <p>Question {`${curQuest + 1}/${numQuestions}`}</p>
       <p>{`${curPoints}/${numPoints}`} Points</p>
     </header>
